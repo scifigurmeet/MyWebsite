d10f89e15324cee41510b67a43f121c850e37fae
scifigurmeet <56145753+scifigurmeet@users.noreply.github.com>
2025-07-15 14:48:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,55 @@
+provider "aws" {
+  region     = "ap-south-1"
+}
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("${path.module}/id_rsa.pub")
+}
+
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0a1235697f4afa8a4"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.allow_http_ssh.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd git
+              systemctl start httpd
+              systemctl enable httpd
+              echo "${base64encode(file("index.html"))}" | base64 -d > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "HTMLServer"
+  }
+}
