450a64a94e7e5c4089e41de9b1288f9a91d79d25
scifigurmeet <56145753+scifigurmeet@users.noreply.github.com>
2025-07-15 15:09:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,60 @@
-provider "aws" {
-  region = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-resource "aws_security_group" "web_sg" {
-  name = "allow_http"
+provider "aws" {
+  region     = var.aws_region
+}
 
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+# Data source to get current AWS account ID
+data "aws_caller_identity" "current" {}
+
+# Data source to get latest Amazon Linux AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# Security group
+resource "aws_security_group" "web_sg" {
+  name_prefix = "web-sg-"
+  
   ingress {
-    description = "Allow HTTP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
   egress {
     from_port   = 0
     to_port     = 0
@@ -21,23 +63,38 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-resource "aws_instance" "web_server" {
-  ami           = "ami-0f5ee92e2d63afc18"  # Amazon Linux 2 (ap-south-1)
-  instance_type = "t2.micro"
-  security_groups = [aws_security_group.web_sg.name]
-
+# EC2 instance
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+  
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  
   user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
-              cat <<EOT > /var/www/html/index.html
-              ${file("index.html")}
-              EOT
-              EOF
-
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+    echo "<h1>Hello from Terraform! Gurmeet Singh</h1>" > /var/www/html/index.html
+  EOF
+  
   tags = {
-    Name = "SimpleHTMLServer"
+    Name = "TerraformWebServer"
   }
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
+output "instance_public_dns" {
+  description = "Public DNS name of the EC2 instance"
+  value       = aws_instance.web.public_dns
+}
+
+output "aws_account_id" {
+  description = "AWS Account ID"
+  value       = data.aws_caller_identity.current.account_id
 }
