75fd0ed31431dbcb27d64d563cc260a04b216a26
scifigurmeet <56145753+scifigurmeet@users.noreply.github.com>
2025-07-15 14:52:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,21 @@
+variable "public_key" {
+  description = "Public SSH key for EC2"
+  type        = string
+}
+
 provider "aws" {
-  region     = "ap-south-1"
+  region = "ap-south-1"
 }
 
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = file("${path.module}/id_rsa.pub")
+  public_key = var.public_key
 }
 
 resource "aws_security_group" "allow_http_ssh" {
-  name        = "allow_http_ssh"
+  name = "allow_http_ssh"
 
   ingress {
-    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -19,7 +23,6 @@ resource "aws_security_group" "allow_http_ssh" {
   }
 
   ingress {
-    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -35,7 +38,7 @@ resource "aws_security_group" "allow_http_ssh" {
 }
 
 resource "aws_instance" "web_server" {
-  ami           = "ami-0a1235697f4afa8a4"
+  ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux 2 in ap-south-1
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer.key_name
   security_groups = [aws_security_group.allow_http_ssh.name]
@@ -43,7 +46,7 @@ resource "aws_instance" "web_server" {
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
-              yum install -y httpd git
+              yum install -y httpd
               systemctl start httpd
               systemctl enable httpd
               echo "${base64encode(file("index.html"))}" | base64 -d > /var/www/html/index.html
